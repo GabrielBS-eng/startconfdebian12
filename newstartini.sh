75a9df818046f8465cfd86da838d600b0c96547a
Gabriel de Brito Silva <britosilva.eng@gmail.com>
2024-04-10 19:05:44 +0200

    Fix history_time for .bashrc

--- newstartini.sh
+++ newstartini.sh
@@ -37,9 +37,10 @@ echo "set -g history-file ~/.tmux_history" >> ~/.tmux.conf
 echo "setw -g mouse on" >> ~/.tmux.conf
 
 # .bashrc extra config
-HISTTIMEFORMAT="%Y-%m-%d %T " >> ~/.bashrc
+time_history="HISTTIMEFORMAT='%Y-%m-%d %T '"
 config_alias="alias ..='cd ..'"
 config_path="PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+grep -qFx "$time_history" ~/.bashrc && echo "history with time already configured" || echo "$time_history" >> ~/.bashrc
 grep -qFx "$config_alias" ~/.bashrc && echo "$config_alias already added" || echo "$config_alias" >> ~/.bashrc
 grep -qFx "$config_path" ~/.bashrc && echo "PATH already configured" || echo "$config_path" >> ~/.bashrc
 
